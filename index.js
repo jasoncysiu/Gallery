ce882b1811ac0e3dcae25305a839918ed0d32a56
SomeGithubNoob <dubbajosh123@gmail.com>
2022-01-25 01:34:19 +1100

    NPM scripts updated

--- index.js
+++ index.js
@@ -14,13 +14,6 @@ const app = express();
 const port = process.env.PORT || 8000;
 const dbURI = process.env.MONGODB_CONNECTION_STRING;
 
-// FOR BODY-PARSER MIDDLEWARE
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-const { validateSearch } = require("./modules/validation");
-const { searchData } = require("./modules/search");
-
 const apiRoutes = require("./routes/apiRoutes");
 
 app.use(bodyParser.json()); // to support JSON-encoded bodies
@@ -45,10 +38,6 @@ db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", function () {
   console.log("Database connected succefully");
 });
-
-const categories = require("./data/categories.json");
-const functionalities = require("./data/functionalities.json");
-const interactionStyles = require("./data/interactionStyles.json");
 
 app.use("/api", apiRoutes);
 
